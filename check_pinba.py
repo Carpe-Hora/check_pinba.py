d3ecbceee30319964e1fa40f413f17a918360a8b
Camille NERON <camille.neron@gmail.com>
2011-07-19 11:30:17 +0200

    first release 0.1

--- check_pinba.py
+++ check_pinba.py
@@ -2,18 +2,30 @@
 
 # Nagios Plugin to check Pinba data
 #
-# Author : Camille NERON camille_neron@carpe-hora.com
+# Author : Camille NERON <camille.neron@gmail.com>
+# Company : Carpe Hora <www.carpe-hora.com>
 #
-# if free to use
+# GNU/GPL v2
 #
 
 #
-# Module defnition
+# Module and variable defnition
 #
 from optparse import OptionParser
+import sys
 import MySQLdb
 
-parser = OptionParser(usage="""%prog 
+# Nagios return codes
+OK = 0
+WARNING = 1
+CRITICAL = 2
+UNKNOWN = 3
+
+#
+# Help and options
+#
+
+parser = OptionParser(usage="""%prog -H db_hostname -u user -p password -D db_name -r table_name -q column_name -v filter_link_with_table_name
 Description :
 
 Pinba is a realtime monitoring/statistics server for PHP using MySQL as a read-only interface. http://pinba.org
@@ -51,7 +63,7 @@ Requirements :
 
 * mysqldb : to Debian aptitude install python-mysqldb""", 
                     
-                    version="%prog 1.0")
+                    version="%prog 0.1")
 
 parser.add_option("-H", "--hostname", dest="hostname", default="localhost",
                   help="mysql hostname \nDefault : localhost", metavar="localhost")
@@ -78,16 +90,19 @@ parser.add_option("-v", "--value", dest="value",
                  help="value on the report type. For example, the app server \"app00\"", metavar="app00")
 
 parser.add_option("-w", "--warning", dest="warning", 
-                  help="warning value, depending of -r, -q and -v options", metavar="5")
+                  help="warning value, depending of -r, -q and -v options", metavar=5)
 
 parser.add_option("-c", "--critical", dest="critical", 
-                  help="critical value, depending of -r, -q and -v options", metavar="10")
+                  help="critical value, depending of -r, -q and -v options", metavar=10)
 
 (options, args) = parser.parse_args()
 
+#
+# Methods
+#
+
 def mysql_connect(hostname, username, password, database, port):
   """connect to the mysql database"""
-
   conn = MySQLdb.connect (host = hostname,
                            user = username,
                            passwd = password,
@@ -113,11 +128,61 @@ def value_column(report):
 def get_data(report, query, value, conn):
   """get data in fonction of the argument (report, query, value) and the connection (conn)"""
   cursor = conn.cursor()
-  print ("query : %s, report : %s, value : %s"  %(query, report, value))
   where_column = value_column(report)
-  cursor.execute ("SELECT %s FROM %s WHERE %s='%s'" %(query, report, where_column, value))
-  row = cursor.fetchone ()
-  print "retour: %s " %row
+  try:
+    cursor.execute ("SELECT %s FROM %s WHERE %s='%s'" %(query, report, where_column, value))
+    row = cursor.fetchone ()
+    return row[0]
+  except:
+    print "error"
+
+
+def end(status, message, perfdata):
+    """Exits the script with the first argument as the return code and the
+       second as the message to generate output."""
+
+    if status == OK:
+        print "OK: %s | %s" % (message, perfdata)
+        sys.exit(0)
+    elif status == WARNING:
+        print "WARNING: %s | %s" % (message, perfdata)
+        sys.exit(1)
+    elif status == CRITICAL:
+        print "CRITICAL: %s | %s" % (message, perfdata)
+        sys.exit(2)
+    else:
+        print "UNKNOWN: %s | %s" % (message, perfdata)
+        sys.exit(3)
+
+def validate_thresholds(warning, critical):
+    """Validates warning and critical thresholds in several ways."""
+
+    if critical != -1 and warning == -2:
+        end(UNKNOWN, "Please also set a warning value when using warning/" +
+	             "critical thresholds!", "")
+    if critical == -1 and warning != -2:
+        end(UNKNOWN, "Please also set a critical value when using warning/" +
+	             "critical thresholds!", "")
+    if critical <= warning:
+        end(UNKNOWN, "When using thresholds the critical value has to be " +
+	              "higher than the warning value. Please adjust your " +
+		      "thresholds.", "")
+
+def return_nagios_status(data, warning, critical, message):
+  """ find the nagios status before use the end methods """
+  validate_thresholds(warning, critical)
+
+  if data < warning:
+    end(OK, message, data)
+  if data >= warning and data < critical:
+    end(WARNING, message, data)
+  if data >= critical:
+    end(CRITICAL, message, data)
+
+
+#
+# Main
+#
 
 conn=mysql_connect(options.hostname, 
                 options.username, 
@@ -125,5 +190,9 @@ conn=mysql_connect(options.hostname,
                 options.database, 
                 options.port)
 
-get_data(options.report, options.query, options.value, conn)
+data = get_data(options.report, options.query, options.value, conn)
+
 mysql_disconnect(conn)
+
+message = options.report + ":" + options.value + " " + str(data)
+return_nagios_status(float(data), float(options.warning), float(options.critical), message)
